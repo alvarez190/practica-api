bc1519f7555e9b234eefa7b2d6bc86df47793ab9
stalin alvarez <stalinrap_123@hotmail.com>
2021-01-18 20:06:39 +0100

    [ADD] practica 1 y 2

--- js/script-p2-video.js
+++ js/script-p2-video.js
@@ -1,5 +1,13 @@
 var node_video = document.getElementById('video');
 
 node_video.addEventListener('contextmenu', (ev) => {
-    document.getElementById('duration').innerHTML = node_video.duration();
+    document.getElementById('duration').innerHTML = "Duracion => " + node_video.duration + " segundos.";
+});
+
+node_video.addEventListener('click', () => {
+    if (!node_video.paused && !node_video.ended) {
+        node_video.pause();
+    } else {
+        node_video.play();
+    }
 });
